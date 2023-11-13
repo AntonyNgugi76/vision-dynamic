b5bf4e04e53dbc532f4c55c1feb7f37cb80a08d5
Antony Ngugi <antonyngugi1276@gmail.com>
2023-11-13 14:15:01 +0300

    repayment

--- lib/antochanges/loan_accounts_Screen.dart
+++ lib/antochanges/loan_accounts_Screen.dart
@@ -1,93 +1,123 @@
-// import 'package:craft_dynamic/antochanges/loan_repayment_history.dart';
-// import 'package:craft_dynamic/craft_dynamic.dart';
-// import 'package:flutter/material.dart';
-// import 'dart:async';
-// import 'dart:convert';
-// import 'package:http/http.dart' as http;
-//
-//
-//
-// class LoanAccounts extends StatefulWidget {
-//   @override
-//   _LoanAccountsState createState() => _LoanAccountsState();
-// }
-//
-// class _LoanAccountsState extends State<LoanAccounts> {
-//   List<Map<String, dynamic>> sampleBankAccounts = [
-//     {"id": 1, "accountNumber": "123456789", "accountType": "Savings"},
-//     {"id": 2, "accountNumber": "987654321", "accountType": "Checking"},
-//     {"id": 3, "accountNumber": "456789012", "accountType": "Savings"},
-//     // Add more sample bank accounts as needed
-//   ];
-//
-//   final _apiService = APIService();
-//   String selectedAccount= '';
-//   List<String> bankAccounts = [];
-//
-//   Future<void> fetchData() async {
-//     // Replace 'your_api_endpoint' with the actual API endpoint
-//     final response = await http.get(Uri.parse('https://your_api_endpoint'));
-//
-//     if (response.statusCode == 200) {
-//       // If the server returns a 200 OK response, parse the data
-//       List<dynamic> data = json.decode(response.body);
-//       List<String> dataList = data.map((item) => item.toString()).toList();
-//
-//       // Set the bankAccounts list to trigger a rebuild
-//       setState(() {
-//         setState(() {
-//           bankAccounts = sampleBankAccounts.map((account) => account['accountNumber'].toString()).toList();
-//         });      });
-//     } else {
-//       // If the server did not return a 200 OK response,
-//       // throw an exception.
-//       throw Exception('Failed to load data');
-//     }
-//   }
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//     fetchData();
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('My Loan Accounts'),
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             // Display a spinner while fetching data
-//             // if (bankAccounts.isEmpty)
-//             //   CircularProgressIndicator()
-//             // else
-//             // Display the DropdownButton with the fetched bank accounts
-//               DropdownButton<String>(
-//                 value: bankAccounts[0], // Set the default value
-//                 onChanged: (String? newValue) {
-//                   setState(() {
-//                     selectedAccount = newValue!;
-//                   });
-//                 },
-//                 items: bankAccounts
-//                     .map<DropdownMenuItem<String>>((String value) {
-//                   return DropdownMenuItem<String>(
-//                     value: value,
-//                     child: Text(value),
-//                   );
-//                 }).toList(),
-//               ),
-//             WidgetFactory.buildButton(context, () => {
-//               // _apiService.getLoanRepaymentHistory();
-//               Navigator.of(context).push(MaterialPageRoute(builder: (context)=>LoanRepaymentHistoryList()))
-//             }, 'Proceed')
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+import 'package:flutter/material.dart';
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoanRepayments(),
+    );
+  }
+}
+
+class Statement {
+  final String id;
+  final String description;
+
+  Statement(this.id, this.description);
+}
+
+class Account {
+  final String id;
+  final String name;
+  final List<Statement> statements;
+
+  Account(this.id, this.name, this.statements);
+}
+
+class StatementListScreen extends StatelessWidget {
+  final List<Statement> statements;
+
+  StatementListScreen(this.statements);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Statements'),
+      ),
+      body: ListView.builder(
+        itemCount: statements.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(statements[index].description),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class LoanRepayments extends StatefulWidget {
+  @override
+  _LoanRepaymentsState createState() => _LoanRepaymentsState();
+}
+
+class _LoanRepaymentsState extends State<LoanRepayments> {
+  List<Account> accounts = [
+    Account(
+      '1',
+      'Account 1',
+      [
+        Statement('1', 'Statement 1'),
+        Statement('2', 'Statement 2'),
+      ],
+    ),
+    Account(
+      '2',
+      'Account 2',
+      [
+        Statement('3', 'Statement 3'),
+        Statement('4', 'Statement 4'),
+      ],
+    ),
+    // Add more accounts with statements as needed
+  ];
+
+  String selectedAccountId = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Account Statements'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Select Account:'),
+            DropdownButton<String>(
+              value: selectedAccountId,
+              onChanged: (String? newValue) {
+                setState(() {
+                  selectedAccountId = newValue!;
+                });
+
+                // Navigate to the StatementListScreen with the selected account's statements
+                Account selectedAccount = accounts.firstWhere(
+                      (account) => account.id == selectedAccountId,
+                  orElse: () => Account('', '', []),
+                );
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => StatementListScreen(selectedAccount.statements),
+                  ),
+                );
+              },
+              items: accounts
+                  .map<DropdownMenuItem<String>>((Account account) {
+                return DropdownMenuItem<String>(
+                  value: account.id,
+                  child: Text(account.name),
+                );
+              }).toList(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
